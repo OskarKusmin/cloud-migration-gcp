5547208b12d42f1dae2cb88ad260a59680252c38
Oskar Kusmin <oskar.kusmin@gmail.com>
2026-07-06 14:35:47 +0300

    Second pipeline test

--- terraform/prod/peering.tf
+++ terraform/prod/peering.tf
@@ -1,7 +1,7 @@
 resource "google_compute_network_peering" "prod_to_test" {
   name         = "prod-to-test"
   network      = module.vpc.network_id
-  peer_network = "projects/${var.test_project_id}/global/networks/voyager-test"
+  peer_network = "projects/${var.peer_project_id}/global/networks/voyager-test"
 
   export_custom_routes = true
   import_custom_routes = true
